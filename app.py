77a04a9d8962d2ea0f075c220f00a81639adbd0f
Vashti259 <156109212+Vashti259@users.noreply.github.com>
2024-03-13 06:26:52 -1100

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from shiny.express import ui, input, render
 import plotly.express as px
 from shiny.express import input, ui
 from shinywidgets import render_plotly
 import palmerpenguins
-ui.page_opts(title="Vashti's layout", fillable=True)
+import seaborn
+
+ui.page_opts(title="Vashti's Unique billing", fillable=True)
 with ui.layout_columns():
+    with ui.sidebar():
+        ui.input_slider("n", "N", 0, 25, 100)
 
-    @render_plotly
-    def plot1():
-        return px.histogram(px.data.tips(), y="tip")
 
-    @render_plotly
-    def plot2():
-        return px.histogram(px.data.tips(), y="total_bill")
+@render.plot(alt="A histogram")
+def histogram():
+    np.random.seed(19680801)
+    x = 25 + 20 * np.random.randn(45)
+    plt.hist(x, input.n(), density=True)
 
+
+from palmerpenguins import load_penguins
+from shiny import render
+from shiny.express import ui
+
+penguins = load_penguins()
+ui.h2("Palmer Penguins")
+
+
+@render.data_frame
+def penguins_df():
+    return render.DataGrid(penguins)
+
+
+@render.plot(alt="A Seaborn histogram on penguin body mass in grams.")
+def plot():
+    ax = seaborn.histplot(data=penguins, x="body_mass_g", bins=input.n())
+    ax.set_title("Palmer Penguins")
+    ax.set_xlabel("Mass (g)")
+    ax.set_ylabel("Count")
+    return ax
